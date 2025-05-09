1f5bedc30730a88f81ae8f57df17f76b00b1fae9
Vasileios <billys01101@gmail.com>
2025-05-09 13:10:09 +0300

    Initial commit with setup files

--- mqtt_pub.py
+++ mqtt_pub.py
@@ -1,5 +1,6 @@
 import sys
 import paho.mqtt.client as paho
+<<<<<<< HEAD
 import ssl
 import time
 import random
@@ -75,3 +76,14 @@ if __name__ == '__main__':
 # Disconnect
 #client.loop_stop()
 #client.disconnect()
+=======
+
+client = paho.Client()
+
+if client.connect("localhost", 1883, 60) != 0:
+    print("Couldn't connect to the mqtt broker")
+    sys.exit(1)
+
+client.publish("test_topic", "Hi, paho mqtt client works fine!", 0)
+client.disconnect()
+>>>>>>> a3ef1b8 (Initial commit with setup files)
--- mqtt_sub.py
+++ mqtt_sub.py
@@ -1,5 +1,6 @@
 import sys
 import paho.mqtt.client as paho
+<<<<<<< HEAD
 import ssl
 import time
 import random
@@ -92,3 +93,30 @@ def run():
 if __name__ == '__main__':
     run()
     
+=======
+
+def message_handling(client, userdata, msg):
+    print(f"{msg.topic}: {msg.payload.decode()}")
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+
+client = paho.Client()
+client.on_message = message_handling
+client.on_connect = on_connect  # Handle connection
+
+if client.connect("localhost", 1883, 60) != 0:
+    print("Couldn't connect to the mqtt broker")
+    sys.exit(1)
+
+client.subscribe("test_topic")
+
+try:
+    print("Press CTRL+C to exit...")
+    client.loop_forever()
+except Exception as e:
+    print(f"Caught an Exception: {e}")
+finally:
+    print("Disconnecting from the MQTT broker")
+    client.disconnect()
+>>>>>>> a3ef1b8 (Initial commit with setup files)
